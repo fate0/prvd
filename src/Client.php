08a961b6c6228eee2aa760c5ea4f0f4fac281234
fate0 <fate0@fatezero.org>
2018-11-01 20:28:09 +0800

    Format fuzzer dsn

--- src/Client.php
+++ src/Client.php
@@ -20,10 +20,72 @@ class PRVD_Fuzzer_Client extends Raven_Client
 {
     public function __construct($options_or_dsn = null, $options = array())
     {
+        if (is_array($options_or_dsn)) {
+            $options = array_merge($options_or_dsn, $options);
+        }
+
+        if (!is_array($options_or_dsn) && !empty($options_or_dsn)) {
+            $dsn = $options_or_dsn;
+        } elseif (!empty($_SERVER['SENTRY_DSN'])) {
+            $dsn = @$_SERVER['SENTRY_DSN'];
+        } elseif (!empty($options['dsn'])) {
+            $dsn = $options['dsn'];
+        } else {
+            $dsn = null;
+        }
+
+        if (!empty($dsn)) {
+            $options = array_merge($options, $this->parseDSN($dsn));
+        }
+
         $options['trace'] = false;
         $options['processors'] = array();
         $options['auto_log_stacks'] = false;
-        parent::__construct($options_or_dsn, $options);
+
+        unset($options['dsn']);
+        unset($_SERVER['SENTRY_DSN']);
+
+        parent::__construct($options);
+    }
+
+    public static function parseDSN($dsn)
+    {
+        switch (strtolower($dsn)) {
+            case '':
+            case 'false':
+            case '(false)':
+            case 'empty':
+            case '(empty)':
+            case 'null':
+            case '(null)':
+                return array();
+        }
+
+        $url = parse_url($dsn);
+        $scheme = (isset($url['scheme']) ? $url['scheme'] : '');
+        if (!in_array($scheme, array('http', 'https'))) {
+            throw new InvalidArgumentException(
+                'Unsupported Sentry DSN scheme: ' .
+                (!empty($scheme) ? $scheme : '<not set>')
+            );
+        }
+
+        $netloc = (isset($url['host']) ? $url['host'] : null);
+        $netloc .= (isset($url['port']) ? ':' . $url['port'] : null);
+        $path = (isset($url['path']) ? $url['path'] : '');
+        $username = (isset($url['user']) ? $url['user'] : null);
+        $password = (isset($url['pass']) ? $url['pass'] : null);
+
+        if (empty($netloc) || empty($username)) {
+            throw new InvalidArgumentException('Invalid Sentry DSN: ' . $dsn);
+        }
+
+        return array(
+            'server' => sprintf('%s://%s%s', $scheme, $netloc, $path),
+            'project' => 0,
+            'public_key' => $username,
+            'secret_key' => $password,
+        );
     }
 
     public function captureRequest()
