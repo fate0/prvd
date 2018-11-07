449be7c086d010d2bce9f944376b1462d43feabc
fate0 <fate0@fatezero.org>
2018-11-07 19:20:36 +0800

    Fixed dir return null bug

--- src/sink/file/001-dir.php
+++ src/sink/file/001-dir.php
@@ -1,6 +1,6 @@
 <?php
 
-function dir($directory, $context=null) {
+function dir($directory, ...$args) {
     prvd_check_path($directory, prvd_translate("Arbitrary File Access"));
-    return call_user_func(PRVD_RENAME_PREFIX."dir", $directory, $context);
+    return call_user_func(PRVD_RENAME_PREFIX."dir", $directory, ...$args);
 }
